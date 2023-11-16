1e4ac4789a01626b0f3c9e9380c49790972a03a3
webdevyoo <yjk7454@naver.com>
2023-11-16 20:03:50 +0900

    fix: BeansFactory의 멤버 필드 beans초기화 메소드 구현 - initialize() <- createInstance() <- findConstructor() 순서로 메소드 기능별 분할 구현

--- src/main/java/org/example/di/BeanFactory.java
+++ src/main/java/org/example/di/BeanFactory.java
@@ -1,8 +1,12 @@
 package org.example.di;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import org.example.annotation.Inject;
+import org.reflections.ReflectionUtils;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+import java.util.*;
 
 public class BeanFactory {
     private final Set<Class<?>> preInstantiatedClazz;
@@ -10,6 +14,61 @@ public class BeanFactory {
 
     public BeanFactory(Set<Class<?>> preInstantiatedClazz) {
         this.preInstantiatedClazz = preInstantiatedClazz;
+        initialize(); // beans 필드 초기화
+    }
+
+    /**
+     * beans 필드 초기화 <br/>
+     * 리플랙션을 통해 초기화 된 preInstantiatedClazz로부터 <br/>
+     * 루프를 통해 순차적으로 beans 맵 겍체에 추가한다.
+     */
+    private void initialize() {
+        for (Class<?> clazz : preInstantiatedClazz) {
+            Object instance = createInstance(clazz);
+            beans.put(clazz, instance);
+        }
+    }
+
+    /**
+     * 인스턴스 생성 메소드 <br/>
+     * 생성한 인스턴스를 기준으로 Map타입 beans필드를 초기화 해야 하므로
+     * beans 필드를 초기화하는 initialize() 메소드 내부에서 호출된다. <br/>
+     * @param clazz
+     * @return
+     */
+    private Object createInstance(Class<?> clazz) {
+
+        //생성자
+        Constructor<?> constructor = findConstructor(clazz); // clazz타입 객체로 Counstructor 조회
+        //파라미터
+        List<Object> parameters = new ArrayList<>();
+        for (Class<?> typeClass : constructor.getParameterTypes()) {
+            parameters.add(getParameterByClass(typeClass)); // 순차적인 parameterTypes에 대한 typeClass 객체에 대한 인스턴스를 가져온다.
+        }
+        //인스턴스 생성
+        try {
+            return constructor.newInstance(parameters.toArray());
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 매개값 클래스타입 객체로 부터 @Inject 어노테이션이 붙은 생성자를 한개 반환해준다.
+     * @param clazz
+     * @return
+     */
+    private Constructor<?> findConstructor(Class<?> clazz) {
+        /**
+         * Reflection에서 제공해주는 ReflectionsUtils의 getAllConstructors() <br/>
+         * 넘겨받은 클래스 타입 객체의 모든 생성자를 가져온다. <br/>
+         * 딘, Inject 어노테이션이 붙은 생성자만 가져온다.
+         */
+        Set<Constructor> injectedConstructors = ReflectionUtils.getAllConstructors(clazz, ReflectionUtils.withAnnotation(Inject.class));
+        if (injectedConstructors.isEmpty()) {
+            return null;
+        }
+        return injectedConstructors.iterator().next(); // 첫번째 요소(생성자)만 리턴한다.
     }
 
     /**
